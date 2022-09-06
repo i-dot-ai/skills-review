d7f8c5cf5fd2109129e60b7e0849bf49ca8f7b35
Ed Singleton <singletoned@gmail.com>
2022-09-06 12:46:49 +0100

    Add script for processing data

---
+++ skills_review/skills/data.py
@@ -0,0 +1,25 @@
+import csv
+import itertools
+
+from skills_review.skills import models
+
+
+def key(row):
+    return (row['level_B_name'], row['level_C_name'], row['clean skill name'])
+
+
+def process_file(file_object):
+    reader = csv.DictReader(file_object)
+    lines = (line for line in reader if line['text'])
+    lines = sorted(lines, key=key)
+    groups = itertools.groupby(lines, key=key)
+
+    for ((name, level_1_name, level_2_name), group) in groups:
+        skill = models.Skill(name=name, level_1_name=level_1_name, level_2_name=level_2_name)
+        skill.save()
+
+        for row in group:
+            sentence = models.SkillSentence(skill=skill, text=row['text'])
+            sentence.save()
+    
+    return lines
---
+++ skills_review/skills/management/commands/process_skills_file.py
@@ -0,0 +1,25 @@
+import pathlib
+
+from django.core.management.base import BaseCommand
+from django.core.files.base import File
+from django.conf import settings
+
+
+from skills_review.skills.data import process_file
+
+
+
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename')
+
+    def handle(self, *args, **options):
+        filename = options['filename']
+        filepath = settings.BASE_DIR / filename
+        with filepath.open() as f:
+            file_object = File(f)
+            lines = process_file(file_object)
+        print(lines[0])
