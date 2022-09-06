caa00ee5d4bea10c365265b11a03bec58d92224b
Ed Singleton <singletoned@gmail.com>
2022-09-06 13:33:37 +0100

    Fix order of fields

--- skills_review/skills/data.py
+++ skills_review/skills/data.py
@@ -5,7 +5,7 @@ from skills_review.skills import models
 
 
 def key(row):
-    return (row['level_B_name'], row['level_C_name'], row['clean skill name'])
+    return (row['level_B_name'], row['level_C_name'], row['clean skill name'], row['skill_name'])
 
 
 def process_file(file_object):
@@ -14,7 +14,8 @@ def process_file(file_object):
     lines = sorted(lines, key=key)
     groups = itertools.groupby(lines, key=key)
 
-    for ((name, level_1_name, level_2_name), group) in groups:
+    for ((level_1_name, level_2_name, clean_name, skill_name), group) in groups:
+        name = clean_name or skill_name
         skill = models.Skill(name=name, level_1_name=level_1_name, level_2_name=level_2_name)
         skill.save()
 
