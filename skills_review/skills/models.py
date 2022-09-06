bf6aa1b7a27648f53d6ca7ad1f14802bfd69198a
Ed Singleton <singletoned@gmail.com>
2022-09-06 11:57:40 +0100

    Add new models

--- skills_review/skills/models.py
+++ skills_review/skills/models.py
@@ -11,8 +11,21 @@ class User(BaseUser):
         super().save(*args, **kwargs)
 
 
+class Skill(models.Model):
+    name = models.CharField(max_length=256, null=True, blank=True)
+
+
+class SkillSentence(models.Model):
+    skill = models.ForeignKey(Skill, related_name="sentences", on_delete=models.CASCADE)
+
+
 class Suggestion(models.Model):
+    class Action(models.TextChoices):
+        NOT_SKILL = ("Not a skill", "Not a skill")
+        CHANGE_NAME = ("Change name", "Change name")
+        NONE = ("None", "None")
+
     user = models.ForeignKey(User, related_name="suggestions", on_delete=models.CASCADE)
-    skill = models.CharField(max_length=256)
-    action = models.CharField(max_length=256)
+    skill = models.ForeignKey(Skill, related_name="suggestions", on_delete=models.CASCADE)
+    action = models.CharField(max_length=256, choices=Action.choices, blank=True, null=True)
     comments = models.TextField()
