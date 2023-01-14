56f70bebe8b995a794701174b31fdf4957aef8a7
Ed Singleton <singletoned@gmail.com>
2023-01-14 11:27:37 +0000

    Convert to pug

--- skills_review/skills/views.py
+++ skills_review/skills/views.py
@@ -51,4 +51,4 @@ async def recommend_skills_from_job_title(request):
     skills = await recommend.get_job_skills(job_title)
     image_url = await recommend.get_job_image_url(job_title)
     context = {'skills': skills, 'job_title': job_title, 'image_url': image_url}
-    return render(request, "recommend.html", context=context)
+    return render(request, "recommend.pug", context=context)
