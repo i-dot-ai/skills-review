a5afbec5944dc24aa95881c5f54ee2e8e2f40cc1
Ed Singleton <singletoned@gmail.com>
2023-01-14 17:20:57 +0000

    Tidy

--- skills_review/skills/migrations/0007_alter_recommendation_image_url.py
+++ skills_review/skills/migrations/0007_alter_recommendation_image_url.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('skills', '0006_auto_20230114_1548'),
+        ("skills", "0006_auto_20230114_1548"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='recommendation',
-            name='image_url',
+            model_name="recommendation",
+            name="image_url",
             field=models.CharField(max_length=2048),
         ),
     ]
