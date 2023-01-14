ef1a4b3df15b616480ef6b7357bdf22823c1c5a3
Ed Singleton <singletoned@gmail.com>
2023-01-14 15:31:31 +0000

    Remove models from admin

--- skills_review/skills/admin.py
+++ skills_review/skills/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Skill)
-admin.site.register(models.SkillSentence)
-admin.site.register(models.Suggestion)
-
 admin.site.register(models.User)
