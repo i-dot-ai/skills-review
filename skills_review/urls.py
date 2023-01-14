75f25b3e368e01af7227b4be68bfb7ac0642532f
Ed Singleton <singletoned@gmail.com>
2023-01-14 15:31:42 +0000

    Remove urls

--- skills_review/urls.py
+++ skills_review/urls.py
@@ -6,8 +6,6 @@ from skills_review.skills import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("recommend-skills/", views.recommend_skills_from_job_title),
-    path("skills", views.skills_view, name="skills"),
-    path("skill/<str:skill_slug>", views.skill_view, name="skill"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ]
