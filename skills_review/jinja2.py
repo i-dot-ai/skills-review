32560a6b2caa809b9ec12dfb24c72b0ccc3e2324
Ed Singleton <singletoned@gmail.com>
2023-01-15 09:46:54 +0000

    Fix url args

--- skills_review/jinja2.py
+++ skills_review/jinja2.py
@@ -36,6 +36,11 @@ def is_selected(data, name, value):
         return ""
 
 
+def url(path, *args, **kwargs):
+    assert not (args and kwargs)
+    return reverse(path, args=args, kwargs=kwargs)
+
+
 def environment(**options):
     extra_options = {"autoescape": True}
     env = jinja2.Environment(
@@ -48,7 +53,7 @@ def environment(**options):
     env.globals.update(
         {
             "static": static,
-            "url": reverse,
+            "url": url,
             "is_selected": is_selected,
             "is_empty_selected": is_empty_selected,
             "DEFAULT": DEFAULT,
