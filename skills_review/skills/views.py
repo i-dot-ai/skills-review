3e3b2553e61cc6028f8937abf1e510f4e251fd2b
Ed Singleton <singletoned@gmail.com>
2023-01-15 09:47:27 +0000

    Remove dedicated POST url

--- skills_review/skills/views.py
+++ skills_review/skills/views.py
@@ -5,10 +5,6 @@ from django.utils.text import slugify
 from . import models, recommend
 
 
-def index_view(request):
-    return render(request, "index.pug")
-
-
 def recommendation_exists(slug):
     return models.Recommendation.objects.filter(slug=slug).exists()
 
@@ -23,14 +19,17 @@ def save_recommendation(job_title, skills, image_url):
     return recommendation
 
 
-async def recommend_skills_from_job_title(request):
-    job_title = request.POST["job-title"]
-    slug = slugify(job_title)
-    if not await sync_to_async(recommendation_exists)(slug):
-        skills = await recommend.get_job_skills(job_title)
-        image_url = await recommend.get_job_image_url(job_title)
-        await sync_to_async(save_recommendation)(job_title, skills, image_url)
-    return redirect(recommendation_view, slug=slug)
+async def index_view(request):
+    if request.method == "GET":
+        return render(request, "index.pug")
+    elif request.method == "POST":
+        job_title = request.POST["job-title"]
+        slug = slugify(job_title)
+        if not await sync_to_async(recommendation_exists)(slug):
+            skills = await recommend.get_job_skills(job_title)
+            image_url = await recommend.get_job_image_url(job_title)
+            await sync_to_async(save_recommendation)(job_title, skills, image_url)
+        return redirect(recommendation_view, slug=slug)
 
 
 def recommendation_view(request, slug):
--- skills_review/urls.py
+++ skills_review/urls.py
@@ -5,7 +5,6 @@ from skills_review.skills import views
 
 urlpatterns = [
     path("", views.index_view, name="index"),
-    path("recommendation/", views.recommend_skills_from_job_title),
     path("recommendation/<str:slug>/", views.recommendation_view),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
