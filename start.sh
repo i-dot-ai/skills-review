de5286c0146139d4931da3437204fa9df28da2d0
Ed Singleton <singletoned@gmail.com>
2023-01-18 19:42:43 +0000

    Generate pickle file at startup

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ set -o errexit
 set -o nounset
 
 python manage.py migrate --noinput
+python manage.py initialise_taxonomy
 watchmedo auto-restart --directory=./  --pattern=""*.py"" --recursive -- python manage.py runserver 0.0.0.0:8008 --noreload
