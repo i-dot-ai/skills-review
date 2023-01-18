ea571e68b7568c5b5e151e7f91f2b308db4f56ca
Ed Singleton <singletoned@gmail.com>
2023-01-18 19:39:20 +0000

    Add commande to initialise skills

---
+++ skills_review/skills/management/commands/initialise_taxonomy.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+from skills_review.skills import taxonomy
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        taxonomy.create_job_title_embeddings()
+        print("Initialised")
