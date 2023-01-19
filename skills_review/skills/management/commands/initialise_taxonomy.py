f39bf3a20d6b0cdf8e520ee71bd134623fd26536
Ed Singleton <singletoned@gmail.com>
2023-01-19 11:16:44 +0000

    Expand print statement

--- skills_review/skills/management/commands/initialise_taxonomy.py
+++ skills_review/skills/management/commands/initialise_taxonomy.py
@@ -6,4 +6,4 @@ from skills_review.skills import taxonomy
 class Command(BaseCommand):
     def handle(self, *args, **options):
         taxonomy.create_job_title_embeddings()
-        print("Initialised")
+        print("Initialised taxonomy")
